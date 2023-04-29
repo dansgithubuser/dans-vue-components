c74a82be23466217a35d94b7c1742509873a1061
Dan <dansonlinepresence@gmail.com>
2023-04-29 14:07:48 -0400

    text input

---
+++ src/components/TextInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="d-text-input">
+    <div>
+      <small>{{ label }}</small>
+    </div>
+    <div>
+      <input
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        v-bind="$attrs"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    modelValue: {},
+    label: String,
+  },
+  emits: ['update:modelValue'],
+}
+
+</script>
--- test/src/App.vue
+++ test/src/App.vue
@@ -1,47 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import TextInput from '../../src/components/TextInput.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <TextInput v-model="sin" label="SIN" @keyup.enter="alert(sin)" />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  data() {
+    return {
+      sin: '',
+      console: window.console,
+    };
+  },
+  methods: {
+    alert(msg) {
+      alert(msg);
+    },
+  },
+};
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
